05acc9a4e9dd6100e1c2a7ded0a55de8b4f1c439
Stojar <95843143+Stojar@users.noreply.github.com>
2021-12-09 08:38:20 +0100

    Add files via upload

---
+++ something.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void main () {
+int stranicaA=2;
+int stranicaB=4;
+int stranicaC=8;
+printf ("Unesi stranicu\n");
+scanf ("%d %d %d",&stranicaA,&stranicaB,&stranicaC);
+int volumen=stranicaA*stranicaB*stranicaC;
+printf ("%d",volumen);
+}
