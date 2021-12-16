c2bec83fb9dfb45be6a1e3e2d1a5b2122dec2070
Stojar <95843143+Stojar@users.noreply.github.com>
2021-12-16 09:30:45 +0100

    Add files via upload

---
+++ račun.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a;
+    int b;
+    int c;
+
+    scanf ("%d", &a);
+    scanf ("%d", &b);
+
+    printf ("površina trokuta je = %d\n",((a*b)/2));
+    return 0;
+}
