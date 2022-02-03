c0bd5683cc66ffba4880f1afd891d13a41feb9a6
Stojar <95843143+Stojar@users.noreply.github.com>
2022-02-03 07:37:02 +0100

    Add files via upload

---
+++ račun zarade.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a;
+    printf("Unesi Markovu zaradu\n");
+    scanf("%d",&a);
+    printf("Marku je preostalo: %d\n",(int)(a*0.512));
+    return 0;
+}
