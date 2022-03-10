97d1bd72c9b5c2b890c86a01f415b3c11d6fed50
Stojar <95843143+Stojar@users.noreply.github.com>
2022-03-10 07:37:03 +0100

    Add files via upload

---
+++ Paran i neparan broj (jednostruko grananje).c
@@ -0,0 +1,22 @@
+
+    #include <stdio.h>
+    #include <stdlib.h>
+
+int main()
+{
+    int a;
+    printf("Unesi jedan broj\n");
+    scanf("%d",&a);
+
+    if (a%2==0)
+    printf("Broj je paran\n");
+
+    if (a%2==1)
+    printf("Broj je neparan\n");
+
+
+
+    return 0;
+}
+
+
