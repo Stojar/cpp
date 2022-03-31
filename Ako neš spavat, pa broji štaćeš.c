af16fefaa70e28832c42963fbe8f0bc079bbf6b1
Stojar <95843143+Stojar@users.noreply.github.com>
2022-03-31 10:21:17 +0200

    Add files via upload

---
+++ Ako neš spavat, pa broji štaćeš.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n;
+    printf("Unesi cijeli broj\n");
+    scanf("%d",&n);
+    while (n){
+        printf("%d...ovca\n",n);
+     n--;
+    }
+    return 0;
+}
