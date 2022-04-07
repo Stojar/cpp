383fece45c3d265a5395fceda6a75c35f75619ec
Stojar <95843143+Stojar@users.noreply.github.com>
2022-04-07 08:01:55 +0200

    Add files via upload

---
+++ razika u ostatcima nakon utrke formula da.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{   int n;
+    float a,b,c,d=1;
+
+    while (d){
+    a=b;
+    b=c;
+    c=d;
+    scanf("%f",&d);
+
+    }
+    printf("razlika: %.2f\n",a-c);
+    return 0;
+}
