6ddc38f62b7a79a2ce9d6006a39f07c460eb24b7
Janković no bozo <95843143+Stojar@users.noreply.github.com>
2022-05-19 10:01:30 +0200

    Add files via upload

---
+++ ispis srednjeg broja sa poljima.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int d=1;
+    int i;
+    int temp;
+    int a[5];
+    for(i=0;i<5;i++){
+        printf("Unesi broj\n");
+        scanf("%d", &a[i]);
+    }while(d){
+        d=0;
+    for(i=0;i<4;i++){
+        if(a[i]>a[i+1]){
+        d=1;
+        temp=a[i];
+        a[i]=a[i+1];
+        a[i+1]=temp;
+        }
+    }
+    }
+
+    printf("%d\n",a[2]);
+    return 0;
+}
