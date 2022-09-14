bcf46f9cd2565f8be0784f2976c0e0ef736aa3f6
Janković no bozo <95843143+Stojar@users.noreply.github.com>
2022-09-14 10:21:35 +0200

    Add files via upload

---
+++ quatro very nice.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int oduzimanje (int x){
+	return 100-x;
+}
+
+int main(){
+int a,b,c,d;
+printf("Koliko su kostale stvari koje je lovro kupio\n");
+scanf("%d",&a);
+printf("Lovro ce dobiti natrag %d eura\n",oduzimanje(a));
+return 0;
+
+    return 0;
+}
