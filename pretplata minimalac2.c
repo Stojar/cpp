322f61097c626c4d1eff955a0e44f60f21656fed
Janković no bozo <95843143+Stojar@users.noreply.github.com>
2022-09-23 10:37:18 +0200

    Add files via upload

---
+++ pretplata minimalac2.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+float trosak_petplate(int N, float x) //definicija funkcije
+{
+return N*x;
+}
+void trosakPretplate2(int *N, float *x, float *rez)
+{
+   *rez=*N * *x;
+}
+/*float trosak_petplate3(int N, float x=15.99)//definicija funkcije
+  {
+      return n*x
+
+      }*/
+
+{
+int main()
+{
+    int brPrijatelja;
+    float cijena, trosak;
+
+     printf("Unesi broj prijatelja: ");
+	scanf("%d", &brPrijatelja);
+
+	printf("Unesi cijenu pretplate: ");
+	scanf("%f", &cijena);
+
+	trosak = trosak_petplate(brPrijatelja, cijena), //poziv funkcije
+    printf("minimalni trosak pretplate iznosi %.2f", trosak);
+
+    trosakPretplate2(&brPrijatelja, &cijena, &trosak);
+     printf("minimalni trosak pretplate iznosi %.2f", trosak);
+    return 0;
+}
