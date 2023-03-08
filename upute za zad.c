947418124e6dfa7be409454bc57835dc92701888
John Šimić <95843143+Stojar@users.noreply.github.com>
2023-03-08 09:25:19 +0100

    Add files via upload

---
+++ upute za zad.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+
+struct gejm{
+    char Character_played[50];
+    int Time_to_complete;
+    int score;
+};
+
+void logTime(){
+//otvor datoteku
+//pitaj èoeka kojeg je èaraktera koristijo, vrijeme igranja i slièno
+//i skor
+//onda to sve apendaj u datoteku
+
+
+logToCharacter(character);
+
+}
+
+void maxScore(){
+//otvor datoteku
+//èitaj dok ima
+//log max score
+}
+
+void totalTimePlayed() {
+    //otvor datoteku
+//èitaj dok ima
+//zbroj vrijeme
+
+
+                     }
+
+void mostUsedCharacter(){
+
+//otvori datoteka popis karaktera
+//petljano otvaraj datoteke svih karaktera i nað max
+
+}
+
+void logToCharacter(char [] character){
+
+//otvori novu datoteku koja se zove character.txt
+//dodaj vrijeme na int koji je zapišan pod tim karakterom
+//also moraš imati datoteku s poopisom karaktera
+}
+
+int main()
+{
+
+    return 0;
+}
