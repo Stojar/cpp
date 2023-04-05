1e2028dee6da843c0bf1eb74bc019c5f38d080ae
John Šimić <95843143+Stojar@users.noreply.github.com>
2023-04-05 10:01:29 +0200

    Add files via upload

---
+++ zekanpekan.cpp
@@ -0,0 +1,50 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<math.h>
+
+struct zeko{
+    int masa;
+	int kolicina_hrane;
+	int darezljivost;
+	char ime[50];
+	}z;
+	
+	void unos(){
+
+	FILE*filePointer;
+	filePointer=fopen("zeko.txt","a");
+
+	printf("How heavy little bunny?\n");
+	scanf("%d", &z.masa);
+	printf("koliko pojede mali zeko?\n");
+	scanf("%d", &z.kolicina_hrane);
+	printf("kolika je darezljivost zekana?\n");
+	scanf("%d", &z.darezljivost	);
+	printf("ime zekanapekana?\n");
+	scanf("%s", z.ime);
+
+	fprintf(filePointer, "%d, %d, %d, %s \n", z.masa, z.kolicina_hrane, z.darezljivost, z.ime);
+	fclose(filePointer);
+}
+	
+	void ispis(){
+
+	FILE*filePointer;
+	filePointer=fopen("zeko.txt","r");
+
+	while(fscanf(filePointer, "%d, %d, %d, %s", &z.masa, &z.kolicina_hrane, &z.darezljivost, z.ime)==4){
+            printf("%d, %d, %d, %s\n\n", z.masa, z.kolicina_hrane, z.darezljivost, z.ime);
+	}
+    
+
+
+
+	fclose(filePointer);
+}
+	int main(){
+	unos();
+	ispis();
+	
+	
+	}
